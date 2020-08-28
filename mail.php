59ede8695a27d2ea999ab8dccedf4f5823e8fc8a
ChrisRoney <70338769+ChrisRoney@users.noreply.github.com>
2020-08-28 21:13:09 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,25 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  
+  $email_from = 'chris.roney@chrisandcarolynsworld.co.uk';
+   
+  $email_subject = "Form from chrisandcarolynsworld.co.uk";
+
+  $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email. \n".
+                     "User Message: $message. \n";
+
+  $to = "chrisroney71@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("Location: index.html"); 
+    
 ?>
